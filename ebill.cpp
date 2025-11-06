8c2b1710eb853479643c2cd7af131c525b3e5fd2
Kadambari Venkata Sai Anmol <11249a138@kanchiuniv.ac.in>
2025-11-05 23:49:37 -0800

    Update ebill.cpp

--- ebill.cpp
+++ ebill.cpp
@@ -21,6 +21,30 @@ cin>>units;
 void calculate()
 {
     if (units<=400)
-    amount
+    amount=0;
+    else if(units<=400)
+    amount=(100*2.25)+(units-200)*4.25;
+    else if(units<=600)
+    amount=(100*2.25)+(200*4.25)+(units-400)*6.25;
+    else
+    amount=(100*2.25)+(200*4.25)+(400*6.25)+(units-500)*9;
+  }
+  void print()
+  {
+    cout<<"\nCustomerid:"<<customerid;
+    cout<<"\nName:"<<customername;
+    cout<<"\nAddress:"<<address;
+    cout<<"\nUnits:"<<units;
+    cout<<"\nBillamount:"<<amount;
+ }
+};
+int main()
+{
+    EBBill b;
+    b.getdetails();
+    b.calculate();
+    b.print();
+    return 0;
 }
-}
+
+
